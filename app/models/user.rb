b99c28991643af884d0361b967c780b1e435ec1c
Paul Storberg <Paul@storberg.net>
2019-09-30 16:43:54 -0700

    added sign-in and sign-out with devise

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  devise :confirmable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "users#show"
+
+  devise_for :users, controllers: {
+    sessions:      "users/sessions",
+    passwords:     "users/passwords",
+    registrations: "users/registrations",
+    confirmations: "users/confirmations"
+  }
+
+  devise_scope :user do
+    get 'sign_in',   to: 'users/sessions#new'
+    get 'sign_up',   to: 'users/registrations#new'
+    delete 'logout', to: 'users/sessions#destroy'
+  end
 end
