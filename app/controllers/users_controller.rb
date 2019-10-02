bffc56ec751ea1133b2d59dd078fdcb2e57ce725
Paul Storberg <Paul@storberg.net>
2019-10-02 11:19:16 -0700

    forum user views updated and forum policy updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def index
-    User.all
+    @users = User.all
   end
 
   def show
     if user_signed_in?
-      @user = current_user
+      @user_founder_forums     = Forum.with_role(:founder, current_user)
+      @user_moderator_forums   = Forum.with_role(:moderator, current_user)
+      @user_participant_forums = Forum.with_role(:participant, current_user)
     else
       redirect_to sign_in_path
     end
