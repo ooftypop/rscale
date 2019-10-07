ec188c4d56d848dba99004119d19d30b141a9278
Paul Storberg <Paul@storberg.net>
2019-10-07 15:28:27 -0600

    minor edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def show
     if user_signed_in?
       @user_admin_polls = Poll.with_role(:resource_admin, current_user)
-      @user_polls        = Poll.with_role(:user, current_user)
+      @user_polls       = Poll.with_role(:user, current_user)
     else
       redirect_to sign_in_path
     end
