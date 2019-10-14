06781d80a3940b8f34c48084dbc24bcfb20c9c8a
Paul Storberg <paulstorberg@Pauls-MacBook-Pro.local>
2019-10-13 21:05:26 -0600

    development file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_10_09_165515) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -37,8 +40,8 @@ ActiveRecord::Schema.define(version: 2019_10_09_165515) do
   end
 
   create_table "polls_groups", force: :cascade do |t|
-    t.integer "poll_id"
-    t.integer "group_id"
+    t.bigint "poll_id"
+    t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_polls_groups_on_group_id"
@@ -48,7 +51,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_165515) do
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
@@ -81,7 +84,7 @@ ActiveRecord::Schema.define(version: 2019_10_09_165515) do
     t.datetime "invitation_accepted_at"
     t.integer "invitation_limit"
     t.string "invited_by_type"
-    t.integer "invited_by_id"
+    t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
@@ -92,8 +95,8 @@ ActiveRecord::Schema.define(version: 2019_10_09_165515) do
   end
 
   create_table "users_groups", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "group_id"
+    t.bigint "user_id"
+    t.bigint "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["group_id"], name: "index_users_groups_on_group_id"
@@ -101,8 +104,8 @@ ActiveRecord::Schema.define(version: 2019_10_09_165515) do
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "role_id"
+    t.bigint "user_id"
+    t.bigint "role_id"
     t.index ["role_id"], name: "index_users_roles_on_role_id"
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
