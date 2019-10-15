d52972924515a63c9fb78733041cb4e121b971f6
Paul Storberg <paulstorberg@Pauls-MacBook-Pro.local>
2019-10-14 21:14:24 -0600

    updates to devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,8 @@ Rails.application.configure do
   config.cache_classes = true
   config.action_mailer.default_url_options = { :host => 'https://rscale.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
