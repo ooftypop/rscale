4e53efabde4f1717f59d131326c6c66bf4145848
Paul Storberg <paulstorberg@Pauls-MacBook-Pro.local>
2019-10-14 21:36:19 -0600

    update action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { :host => 'https://rscale.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://rscale.herokuapp.com', from: 'no.response.rscale@gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
