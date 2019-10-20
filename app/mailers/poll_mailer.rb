fd839ace1257e3e3333860aa9a8d14b61e589477
Paul Storberg <paul@storberg.net>
2019-10-20 15:54:12 -0600

    minor edit

--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -5,7 +5,7 @@ class PollMailer < ActionMailer::Base
   def poll_invite_email(user, poll)
     @user = user
     @poll = poll
-    @url  = "#{link_to "#{@poll.title}", poll_path(@poll)}"
+    @url  = "#{poll_url(@poll)}"
     mail(to: @user.email, subject: 'Invitation to vote')
   end
 end
