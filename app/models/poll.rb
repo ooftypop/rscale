093aa96f99fd58d6bb665495c93e773f259b2cd9
Paul Storberg <paul@storberg.net>
2019-10-21 16:45:25 -0600

    created logic to turn active to fasle upon all users voting

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -3,10 +3,34 @@ class Poll < ApplicationRecord
 
   has_many :poll_options, dependent: :destroy
   has_many :polls_groups
-
   has_many :groups, through: :polls_groups
 
   validates :title, presence: true
 
   accepts_nested_attributes_for :poll_options, reject_if: lambda {|attributes| attributes['title'].blank?}
+
+  def timed?
+
+  end
+
+  def all_users_voted?
+    user_votes         = 0
+    total_user_votes   = 0
+    roles_count        = 0
+    total_poll_options = self.poll_options.count
+
+    self.roles.each do |role|
+      roles_count = roles_count + role.users.count
+    end
+
+    self.poll_options.each do |poll_option|
+      user_votes = user_votes + poll_option.votes.count
+    end
+
+    total_avaliable_votes = roles_count * total_poll_options
+
+    if total_avaliable_votes == user_votes
+      self.update(active: false)
+    end
+  end
 end
