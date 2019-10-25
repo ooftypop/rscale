855228b43dccabd50bd17743b7f4dc435d3f3a7f
Paul Storberg <paul@storberg.net>
2019-10-25 12:48:32 -0600

    added slugs to users and polls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_184346) do
+ActiveRecord::Schema.define(version: 2019_10_25_182812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,17 @@ ActiveRecord::Schema.define(version: 2019_10_24_184346) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -59,6 +70,8 @@ ActiveRecord::Schema.define(version: 2019_10_24_184346) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean "active", default: true
+    t.string "slug"
+    t.index ["slug"], name: "index_polls_on_slug", unique: true
   end
 
   create_table "polls_groups", force: :cascade do |t|
@@ -108,12 +121,14 @@ ActiveRecord::Schema.define(version: 2019_10_24_184346) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
   create_table "users_groups", force: :cascade do |t|
