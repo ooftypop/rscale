8726d9699371cebceaa0e51d8511d4c1081d741e
Paul Storberg <paul@storberg.net>
2019-11-06 18:53:08 -0500

    complete overhaul of apps design

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   def new
-    super
+    render "sign_in_form.js"
   end
 
   def create
