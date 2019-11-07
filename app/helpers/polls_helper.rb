28df5eb182de39d471843248002c79552496c470
Paul Storberg <paul@storberg.net>
2019-11-07 10:07:51 -0500

    added new button style to remaining actions

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,9 +1,9 @@
 module PollsHelper
   def user_vote_button(poll)
     if current_user.voted_for_poll?(poll)
-      button_tag("You Voted!", class: ["submit-button", "submit-all-button"], type: "button")
+      button_tag("You Voted!", class: ["btn", "btn-primary", "js-scroll-trigger"], type: "button")
     else
-      button_tag("Vote", class: ["submit-button", "submit-all-button"], name: "button", type: "button", id: "masterSave", onclick: "submitAllVotes()" )
+      button_tag("Vote", class: ["btn", "btn-primary", "js-scroll-trigger"], name: "button", type: "button", id: "masterSave", onclick: "submitAllVotes()" )
     end
   end
 end
