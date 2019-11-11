7e323b2ede354a7755c887f8e36ff60c2623c05c
Paul Storberg <paul@storberg.net>
2019-11-11 16:21:42 -0500

    minor edit

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,17 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def create
-    super
+    self.resource = warden.authenticate(auth_options)
+  
+    if self.resource
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      redirect_to root_path 
+      flash[:alert] = "Incorrect email or password."
+    end
   end
 
   def destroy
