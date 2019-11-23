c9155b794519746d6acf1f1a35856b7a07f3b399
Kevin J. Storberg <kevin@storberg.net>
2019-11-22 16:13:30 -0800

    temporarily removed styling on navbar, fixed issue with navbar dropdown, placed main-content yield into container, created nav_classes helper method in newly created layouts_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  
+
   def resource_name
     :user
   end
- 
+
   def resource
     @resource ||= User.new
   end
@@ -11,7 +11,7 @@ module ApplicationHelper
   def resource_class
     User
   end
- 
+
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
