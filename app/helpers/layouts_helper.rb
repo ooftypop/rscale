e585d79f929fee323d58f096241b3703ae3acfd3
Kevin J. Storberg <kevin@storberg.net>
2019-11-24 20:10:25 -0800

    used helper method with navbar

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -1,7 +1,7 @@
 module LayoutsHelper
 
   def nav_classes
-    classes = "navbar navbar-expand-lg navbar-light fixed-top"
+    classes = "navbar navbar-expand-lg navbar-light fixed-top "
     classes = classes + 'signed-in-navbar' if ['static_pages'].exclude?(controller_name)
 
     return classes
